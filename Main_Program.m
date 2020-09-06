879e6548c44c774ce4a4f4345fb0f8cfeded821f
Mojtaba Masoudinejad <mojtaba.masoudinejad@tu-dortmund.de>
2020-09-06 17:51:03 +0200

    Main program with all processes

---
+++ Main_Program.m
@@ -0,0 +1,16 @@
+%     InitCycler % run initial cycling to prepare the battery
+% pause(5*60) % wait
+
+% BatteryTester %test the battery
+% pause(5*60) % wait
+%     Battery_VocMeasure
+% pause(5*60) % wait
+% BatteryTester %test the battery
+% pause(5*60) % wait
+%     Battery_RandomLoadTest
+% pause(5*60) % wait
+% BatteryTester %test the battery
+% pause(5*60) % wait
+    SlowDischarge
+% pause(5*60) % wait
+% BatteryTester %test the battery
