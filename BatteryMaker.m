bf3fbcfd26067e68da092a8f1bbd88a31b14f00d
Mojtaba Masoudinejad <mojtaba.masoudinejad@tu-dortmund.de>
2020-09-10 12:51:02 +0200

    Extra information for battery maker

--- BatteryMaker.m
+++ BatteryMaker.m
@@ -2,14 +2,16 @@
 %  User is supposed to modify these information according to the battery
 %  To enable measurement of different battery from same series there is a single input to this function
 %  Input has to be an integer related to the battery number
-function Battery = BatteryMaker(Item)
-Battery = struct; % Make an empty object
+function Battery = BatteryMaker(Bat_No)
+Battery = struct;
 Battery.Manufacturer = 'Shenzhen Blue TaiYang new energy technology Co., LTD';
 Battery.Model = 'LP632670';
-Battery.Item = Item;
+Battery.Item = Bat_No;
 Battery.Capacity = 1.25; % capacity in [Ah]
 Battery.VMax = 4.2; % Maximum allowed voltage in [V]
 Battery.VNom = 3.7; % nominal voltage of the battery in [V]
+Battery.VBreak = 3.48; % voltage which the Voc curve breaks to hyperbolic [V]
 Battery.VMin = 3.0; % cut off voltage of the battery in [V]
 Battery.IStandardRatio = 0.20; % standard current according to the C in [ratio]
+Battery.IInfiniRatio = 0.01; % very small C-rate considered as infinitesimal current in [ratio]
 Battery.IMaxRatio = 1; % maximum allowed current according to the C in [ratio]
